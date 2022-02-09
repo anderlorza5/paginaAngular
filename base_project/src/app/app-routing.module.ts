560dfd05abad459e57f4dd82679dac1f75e62600
Ander <ander210194@gmail.com>
2022-02-09 21:22:33 +0100

    mejorado algunas cosas

--- base_project/src/app/app-routing.module.ts
+++ base_project/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ComponenteComponent } from './components/componente/componente.component';
-import {CarruselComponent} from './components/carrusel/carrusel.component'
+import {CarruselComponent} from './components/carrusel/carrusel.component';
+import { CompraComponent } from './pages/compra/compra.component';
+import { HomeComponentComponent } from './pages/home/home-component/home-component.component';
+
 
 const routes: Routes = [
-  {path: "hola",
-  component: CarruselComponent}
+  {path: '',
+  component: HomeComponentComponent},
+  {path: 'hola',
+  component: CompraComponent}
 ];
 
 @NgModule({
--- base_project/src/app/pages/compra/compra.component.ts
+++ base_project/src/app/pages/compra/compra.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-compra',
@@ -10,7 +11,7 @@ export class CompraComponent implements OnInit {
 
 
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
